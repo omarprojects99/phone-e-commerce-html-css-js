90ba33c28598586f44cf4c7c8e2a75d55cfa1eb7
omarprojects99 <omarbenhajsaid1@gmail.com>
2024-12-10 18:33:03 +0100

    changement

--- script.js
+++ script.js
@@ -32,4 +32,17 @@ function displayProductCards(products) {
     // Append the card to the grid container
     productGrid.appendChild(productCard);
   });
+  
 }
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuOpen = document.getElementById('mobile-menu-open');
+const mobileMenuClose = document.getElementById('mobile-menu-close');
+
+mobileMenuOpen.addEventListener('click', () => {
+  mobileMenu.classList.remove('hidden');
+});
+
+mobileMenuClose.addEventListener('click', () => {
+  mobileMenu.classList.add('hidden');
+});
+
